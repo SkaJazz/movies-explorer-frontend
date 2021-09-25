ff347543a839a2f6fb34287a7f78b11a16237607
Mikhail Malyarov <remaik@gmail.com>
2021-09-25 09:55:35 +0300

    feat: добавление роутов

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route path="/movies"></Route>
+          <Route path="/saved-movies"></Route>
+          <Route path="/profile"></Route>
+          <Route path="/signin"></Route>
+          <Route path="/signup"></Route>
+          <Route path="/signup"></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
