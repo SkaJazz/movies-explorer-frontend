57e8de998273183b7f76a1faa617f66fd4c76618
SkaJazz <remaik@gmail.com>
2021-11-06 15:36:01 +0300

    feat: завершение блока About Project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,7 +4,7 @@ import ComponentHeader from '../ComponentHeader/ComponentHeader';
 export default function AboutProject(props) {
   return (
     <section className="about">
-      <ComponentHeader title="О проекте"/>
+      <ComponentHeader title="О проекте" />
       <ul className="about__cards">
         <li className="about__card">
           <h3 className="about__card-header">
@@ -23,6 +23,16 @@ export default function AboutProject(props) {
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
             соблюдать, чтобы успешно защититься.
           </p>
+        </li>
+      </ul>
+      <ul className="timeline">
+        <li className="timeline__element timeline__element--back">
+          <p className="timeline__line timeline__line--back">1 неделя</p>
+          <p className="timeline__label">Back-end</p>
+        </li>
+        <li className="timeline__element timeline__element--front">
+          <p className="timeline__line">4 недели</p>
+          <p className="timeline__label">Front-end</p>
         </li>
       </ul>
     </section>
