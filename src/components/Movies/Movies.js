6018c745c672f95e5f8a1b0033507a3091b3ea41
SkaJazz <remaik@gmail.com>
2022-01-29 12:16:24 +0300

    feat: добавление фильтра фильмов

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,30 @@
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import SearchForm from "../SearchForm/SearchForm";
+
+import movies from "../../vendor/mock_movies.json";
+import { useState } from "react";
 
 export default function Movies() {
-  return(
+  const filmList = movies.slice(0, 100);
+  const [filteredFilmList, setFilteredFilmList] = useState(filmList);
+  console.log(filteredFilmList);
+
+  const handleSearchSubmit = ({ searchString }) => {
+    const trimmedSearchString = searchString.trim();
+    if (trimmedSearchString) {
+      const filteredFilms = filmList.filter((film) =>
+        film.nameRU.toLowerCase().includes(trimmedSearchString.toLowerCase())
+      );
+      setFilteredFilmList(filteredFilms);
+    } else {
+      setFilteredFilmList(filmList);
+    }
+  };
+
+  return (
     <>
-      <MoviesCardList />
+      <SearchForm handleSearchSubmit={handleSearchSubmit} />
+      <MoviesCardList films={filteredFilmList} />
     </>
-  )
-};
+  );
+}
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,38 @@
-import search_icon from "../../images/logo.svg";
+import search_icon from "../../images/search_icon.svg";
+import { useState } from "react";
 
-export default function SearchForm(params) {
+export default function SearchForm({ handleSearchSubmit }) {
+  const [searchString, setSearchString] = useState("");
+
+  const handleSearchStringChange = (e) => setSearchString(e.target.value);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchString);
+    handleSearchSubmit({
+      searchString
+    });
+  };
+
   return (
-    <form action="post" className="search-form">
-      <label htmlFor="search-form__film-input" className="search-form__label">
-        {search_icon}
+    <form action="post" className="search-form" onSubmit={onFormSubmit}>
+      <label htmlFor="search-input" className="search-form__label">
+        <img
+          alt="Иконка поиска"
+          className="search-form__film-input-logo"
+          src={search_icon}
+        />
       </label>
-      <input type="text" className="search-form__film-input" />
+      <input
+        type="text"
+        placeholder="Фильм"
+        autoсomplete="off"
+        value={searchString || ""}
+        onChange={handleSearchStringChange}
+        name="search-input"
+        id="search-input"
+        className="search-form__film-input"
+      />
     </form>
   );
 }
