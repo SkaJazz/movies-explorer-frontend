d4c4a6a6d986bd3dc6f8ca994c6ce828decb5f13
SkaJazz <remaik@gmail.com>
2022-01-30 11:12:08 +0300

    feat: добавление компонента чекбокса

---
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -0,0 +1,16 @@
+import './FilterCheckbox.css';
+
+export default function FilterCheckbox({isShortsOnly, toggleIsShortsOnly}) {
+  return (
+    <label htmlFor="toggler" className="switch">
+      <input
+        type="checkbox"
+        role="switch"
+        id="toggler"
+        checked={!!isShortsOnly}
+        onChange={toggleIsShortsOnly}
+      />
+      Короткометражки
+    </label>
+  );
+}
