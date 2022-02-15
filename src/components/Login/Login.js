09155fc2189409d504eff8bceaa06fb4c8b6d19c
SkaJazz <remaik@gmail.com>
2022-02-15 21:31:12 +0300

    feat: добавление валидации к форме логина

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,26 +7,47 @@ export default function Login({handleLogin}) {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [errorObject, setErrorObject] = useState({
-    nameErrMsg: "",
-    emailErrMsg: "",
-    pwdErrMsg: "",
+    emailErrMsg: "Введите email",
+    pwdErrMsg: "Введите пароль",
   });
+
+    const checkEmail = (email) => {
+      setUserEmail(email);
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        setErrorObject({ ...errorObject, emailErrMsg: "Введите email" });
+      } else {
+        setErrorObject({ ...errorObject, emailErrMsg: "" });
+      }
+    };
+
+    const checkPwd = (pwd) => {
+      setUserPassword(pwd);
+      pwd.length < 2
+        ? setErrorObject({ ...errorObject, pwdErrMsg: "Введите пароль" })
+        : setErrorObject({ ...errorObject, pwdErrMsg: "" });
+    };
 
   return (
     <Section className="sign-section">
       <SignForm
         type="login"
-        submitHandler={() => handleLogin({
-          email: userEmail,
-          password: userPassword,
-        })}
+        submitHandler={() =>
+          handleLogin({
+            email: userEmail,
+            password: userPassword,
+          })
+        }
+        hasErrors={
+          errorObject.nameErrMsg ||
+          errorObject.emailErrMsg ||
+          errorObject.pwdErrMsg
+        }
       >
         <InputLine
           label="E-mail"
           inputName="email-input"
           type="email"
-          onChange={(e) => setUserEmail(e.target.value.trim())}
-          onFocus={() => setErrorObject({ ...errorObject, emailErrMsg: "" })}
+          onChange={(e) => checkEmail(e.target.value.trim())}
           value={userEmail}
           errorMessage={errorObject.emailErrMsg}
         />
@@ -34,8 +55,7 @@ export default function Login({handleLogin}) {
           label="Пароль"
           inputName="password-input"
           type="password"
-          onChange={(e) => setUserPassword(e.target.value.trim())}
-          onFocus={() => setErrorObject({ ...errorObject, pwdErrMsg: "" })}
+          onChange={(e) => checkPwd(e.target.value.trim())}
           value={userPassword}
           errorMessage={errorObject.pwdErrMsg}
         />
