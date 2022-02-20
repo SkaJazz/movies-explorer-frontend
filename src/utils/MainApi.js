81d0eacc1d0f5b7bcff0b43d9b4d9c00c24f6c1a
SkaJazz <SkaJazz@users.noreply.github.com>
2022-02-20 13:59:45 +0300

    fix: исправление аутофиксимых ошибок в api-шном классе

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -6,7 +6,7 @@ class MainApi {
   _sendRequest = ({
     method,
     path,
-    contentType = "application/json",
+    contentType = 'application/json',
     body = null,
     authToken = null,
   }) =>
@@ -14,19 +14,17 @@ class MainApi {
       method,
       headers: {
         authorization: authToken,
-        "Content-Type": contentType,
+        'Content-Type': contentType,
       },
       body,
-    }).then((res) => this._checkServerResponseMiddleware(res));
+    }).then(res => this._checkServerResponseMiddleware(res));
 
-  _checkServerResponseMiddleware = (res) => {
-    return res.ok ? res.json() : Promise.reject(res.statusText);
-  };
+  _checkServerResponseMiddleware = res => res.ok ? res.json() : Promise.reject(res.statusText);
 
   signUp = ({ email, name, password }) =>
     this._sendRequest({
-      method: "POST",
-      path: "signup",
+      method: 'POST',
+      path: 'signup',
       body: JSON.stringify({
         password,
         email,
@@ -36,54 +34,52 @@ class MainApi {
 
   signIn = ({ email, password }) =>
     this._sendRequest({
-      method: "POST",
-      path: "signin",
+      method: 'POST',
+      path: 'signin',
       body: JSON.stringify({
         password,
         email,
       }),
     });
 
-  getUserInfo = (token) =>
+  getUserInfo = token =>
     this._sendRequest({
-      method: "GET",
-      path: "users/me",
+      method: 'GET',
+      path: 'users/me',
       authToken: `Bearer ${token}`,
     });
 
   updateUser = ({ token, userData }) =>
     this._sendRequest({
-      method: "PATCH",
-      path: "users/me",
+      method: 'PATCH',
+      path: 'users/me',
       authToken: `Bearer ${token}`,
       body: JSON.stringify(userData),
     });
 
-  getMovies = (token) => 
+  getMovies = token =>
     this._sendRequest({
-      method: "GET",
-      path: "movies",
+      method: 'GET',
+      path: 'movies',
       authToken: `Bearer ${token}`,
     });
   ;
 
-  postMovie = ({ token, movieDataToRequest }) => {
-    return this._sendRequest({
-      method: "POST",
-      path: "movies",
-      authToken: `Bearer ${token}`,
-      body: JSON.stringify(movieDataToRequest),
-    });
-  };
+  postMovie = ({ token, movieDataToRequest }) => this._sendRequest({
+    method: 'POST',
+    path: 'movies',
+    authToken: `Bearer ${token}`,
+    body: JSON.stringify(movieDataToRequest),
+  });
 
-  removeMovie = (token, movieId) => 
+  removeMovie = (token, movieId) =>
     this._sendRequest({
-      method: "DELETE",
+      method: 'DELETE',
       path: `movies/${movieId}`,
       authToken: `Bearer ${token}`,
     });
 }
 
-const mainApi = new MainApi("https://api.nomoredomains.club");
+const mainApi = new MainApi('https://api.nomoredomains.club');
 
 export default mainApi;
