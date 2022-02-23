5b0a750a3fcc9df6528779d74e1951a43f9a1096
SkaJazz <SkaJazz@users.noreply.github.com>
2022-02-23 23:24:33 +0300

    ref: рефакторинг компонента регистрации

--- src/components/InputLine/InputLine.js
+++ src/components/InputLine/InputLine.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './InputLine.css';
 
 export default function InputLine({
-  label, inputName, type, value, onChange, errorMessage, onFocus
+  label,
+  inputName,
+  type,
+  value,
+  onChange,
+  errorMessage,
+  name,
+  required,
+  minLength,
+  maxLength,
+  pattern
 }) {
   return (
     <p className="input__container">
@@ -10,12 +20,16 @@ export default function InputLine({
         {label}
       </label>
       <input
+        minLength={minLength}
+        pattern={pattern}
+        maxLength={maxLength}
+        required={required}
         autoComplete="off"
         onChange={onChange}
         type={type}
+        name={name}
         className={`input ${errorMessage ? 'input_error' : ''}`}
         value={value || ''}
-        onFocus={onFocus}
       />
       {errorMessage && (
         <span className="input__error-message">{errorMessage}</span>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,55 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Section from '../Section/Section';
 import SignForm from '../SignForm/SignForm';
 import InputLine from '../InputLine/InputLine';
+import useFormWithValidation from '../FormValidator/FormValidator';
 
 export default function Register({ handleRegister }) {
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userPassword, setUserPassword] = useState('');
-  const [errorObject, setErrorObject] = useState({
-    nameErrMsg: 'Введите имя',
-    emailErrMsg: 'Введите email',
-    pwdErrMsg: 'Введите пароль',
-  });
-
-  const checkName = name => {
-    setUserName(name);
-    if (name.length < 3) {
-      setErrorObject({
-        ...errorObject,
-        nameErrMsg: 'Имя должно быть длиннее двух символов',
-      });
-    } else if (name.length > 30) {
-      setErrorObject({
-        ...errorObject,
-        nameErrMsg: 'Имя должно быть короче 30 символов',
-      });
-    } else if (!/^[а-яА-Яa-zA-Z -]+$/.test(name)) {
-      setErrorObject({
-        ...errorObject,
-        nameErrMsg: 'Имя может содержать только буквы, пробел и дефис',
-      });
-    } else {
-      setErrorObject({ ...errorObject, nameErrMsg: '' });
-    }
-  };
-
-  const checkEmail = email => {
-    setUserEmail(email);
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      setErrorObject({ ...errorObject, emailErrMsg: 'Введите email' });
-    } else {
-      setErrorObject({ ...errorObject, emailErrMsg: '' });
-    }
-  };
-
-  const checkPwd = pwd => {
-    setUserPassword(pwd);
-    pwd.length < 2
-      ? setErrorObject({ ...errorObject, pwdErrMsg: 'Введите пароль' })
-      : setErrorObject({ ...errorObject, pwdErrMsg: '' });
-  };
+  // NEW
+  const {
+    handleChange, values, isValid, errors
+  } = useFormWithValidation();
 
   return (
     <Section className="sign-section">
@@ -57,39 +16,46 @@ export default function Register({ handleRegister }) {
         type="reg"
         submitHandler={() =>
           handleRegister({
-            email: userEmail,
-            name: userName,
-            password: userPassword,
+            email: values.userEmail,
+            name: values.userName,
+            password: values.userPassword,
           })}
-        hasErrors={
-          errorObject.nameErrMsg
-          || errorObject.emailErrMsg
-          || errorObject.pwdErrMsg
-        }
+        hasErrors={!isValid}
       >
         <InputLine
+          required
+          minLength={5}
+          maxLength={30}
+          pattern="[а-яА-Яa-zA-Z -]+"
           label="Имя"
+          name="userName"
           inputName="name-input"
           type="text"
-          onChange={e => checkName(e.target.value.trim())}
-          value={userName}
-          errorMessage={errorObject.nameErrMsg}
+          onChange={e => handleChange(e)}
+          value={values.userName}
+          errorMessage={errors.userName}
         />
         <InputLine
+          required
+          pattern="[^\s@]+@[^\s@]+\.[^\s@]+"
           label="E-mail"
           inputName="email-input"
+          name="userEmail"
           type="email"
-          onChange={e => checkEmail(e.target.value.trim())}
-          value={userEmail}
-          errorMessage={errorObject.emailErrMsg}
+          onChange={e => handleChange(e)}
+          value={values.userEmail}
+          errorMessage={errors.userEmail}
         />
         <InputLine
+          required
+          minLength={2}
           label="Пароль"
           inputName="password-input"
+          name="userPassword"
           type="password"
-          onChange={e => checkPwd(e.target.value.trim())}
-          value={userPassword}
-          errorMessage={errorObject.pwdErrMsg}
+          onChange={e => handleChange(e)}
+          value={values.userPassword}
+          errorMessage={errors.userPassword}
         />
       </SignForm>
     </Section>
